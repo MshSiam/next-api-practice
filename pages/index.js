ba70b16ad04689c0d29c61b4c7f41d662480a847
MshSiam <siyamkenway@gmail.com>
2022-07-29 21:34:33 +0600

    comment page

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ function Home() {
       <h1>This is Next Api CRUD practice.</h1>
 
       <button>
-        <Link href="/comments">Comments</Link>
+        <Link href="/Comments">Comments</Link>
       </button>
     </>
   )
